cfc572547b014e5f41dd588ab55ab0b90ded78b8
Nemzytch <lesieutre.kevin@gmail.com>
2023-12-13 01:27:05 +0100

    example.com test

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
+import requests
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return requests.get('http:example.com').text
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
